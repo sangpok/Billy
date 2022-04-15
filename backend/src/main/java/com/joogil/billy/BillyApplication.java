de4b27799870c4cec6277a3eb242ca6e5562e477
kgw012 <kgw012@gmail.com>
2022-04-15 19:47:53 +0900

    Add backend skeleton code

---
+++ backend/src/main/java/com/joogil/billy/BillyApplication.java
@@ -0,0 +1,13 @@
+package com.joogil.billy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillyApplication.class, args);
+	}
+
+}
