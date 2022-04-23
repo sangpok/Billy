6efe16312714e30a25f83343eb897ef1d7d09c88
kgw012 <kgw012@gmail.com>
2022-04-23 17:34:59 +0900

    Update spring security username&password, Update .gitignore

--- backend/src/main/java/com/joogil/billy/BillyApplication.java
+++ backend/src/main/java/com/joogil/billy/BillyApplication.java
@@ -3,9 +3,13 @@ package com.joogil.billy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySource(value = { "classpath:mysql.properties" })
+@PropertySources({
+	@PropertySource("classpath:secrets/mysql.properties"),
+	@PropertySource("classpath:secrets/security.properties")
+})
 public class BillyApplication {
 
 	public static void main(String[] args) {
