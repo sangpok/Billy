c39ce52d88da57ed0a95130ee753fc22e7f2db77
sangpok <y_y5760y_y@naver.com>
2022-05-07 21:03:21 +0900

    Add Login Page

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>asasdfdf</div>
+</template>
+
+<script>
+    export default {
+        name: 'LoginPage',
+        components: {},
+        data() {
+            return {
+                sampleData: '',
+            };
+        },
+        setup() {},
+        created() {},
+        mounted() {},
+        unmounted() {},
+        methods: {},
+    };
+</script>
